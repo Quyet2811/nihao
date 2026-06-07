ba4d2b32e0ae0bba8daae0416745afcffa3b07c6
Quyet2811 <nguyentquyet007@gmail.com>
2026-06-08 01:56:38 +0700

    Update tmu.js

--- db/tmu.js
+++ db/tmu.js
@@ -86,238 +86,238 @@ UNIVERSITY_DATABASE["TMU"] = {
         "TM01": {
             name: "Quản trị kinh doanh",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.8, hsa: 105.333 } 
         },
          "TM02": {
             name: "Quản trị kinh doanh - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.4, hsa: 102.667 } 
         },
          "TM03": {
             name: "Quản trị kinh doanh (Khởi nghiệp và phát triển kinh doanh) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.1, hsa: 100.667 } 
         },
          "TM04": {
             name: "Quản trị khách sạn ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.4, hsa:102.667  } 
         },
          "TM05": {
             name: "Quản trị khách sạn (Quản trị khách sạn - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.1, hsa: 100.667 } 
         },
          "TM06": {
             name: "Quản trị dịch vụ du lịch và lữ hành ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.8, hsa: 99.12 } 
         },
          "TM07": {
             name: "Marketing (Marketing Thương mại) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 27.3, hsa: 115.333 } 
         },
          "TM08": {
             name: "Marketing (Marketing thương mại - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.3, hsa: 108.667 } 
         },
          "TM09": {
             name: "Marketing (Marketing số) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 27.5, hsa: 116.667 } 
         },
          "TM10": {
             name: "Marketing (Quản trị Thương hiệu) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.8, hsa: 112 } 
         },
          "TM11": {
             name: "Marketing (Quản trị thương hiệu - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.1, hsa: 107.333 } 
         },
          "TM12": {
             name: "Kế toán (Kế toán doanh nghiệp) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.8, hsa: 105.333 } 
         },
          "TM13": {
             name: "Kế toán (Kế toán tích hợp chứng chỉ quốc tế ICAEW CFAB - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24, hsa: 95.6 } 
         },
          "TM14": {
             name: " Kế toán (Kế toán công)",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.8, hsa: 99.12 } 
         },
         "TM15": {
             name: "Kiểm toán (Kiểm toán) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.7, hsa: 104.667 } 
         },
         "TM16": {
             name: "Kiểm toán (Kiểm toán tích hợp chứng chỉ quốc tế ICAEW CFAB - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24, hsa: 95.6 } 
         },
         "TM17": {
             name: "Logistics và Quản lý chuỗi cung ứng (Logistics và Quản lý chuỗi cung ứng) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 27.8, hsa: 118.667 } 
         },
         "TM18": {
             name: "Logistics và Quản lý chuỗi cung ứng (Logistics và Xuất nhập khẩu - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.6, hsa: 110.667 } 
         },
         "TM19": {
             name: "Kinh doanh quốc tế (Thương mại quốc tế) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.8, hsa: 112 } 
         },
         "TM20": {
             name: "Kinh doanh quốc tế (Thương mại quốc tế - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.3, hsa: 108.667 } 
         },
         "TM21": {
             name: " Kinh tế quốc tế",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.6, hsa: 110.667 } 
         },
         "TM22": {
             name: "Kinh tế (Quản lý kinh tế) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.1, hsa: 100.667 } 
         },
         "TM23": {
             name: "Kinh tế (Kinh tế và Quản lý đầu tư - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.7, hsa: 98.68 } 
         },
         "TM24": {
             name: " Tài chính - Ngân hàng (Tài chính - Ngân hàng thương mại)",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.1, hsa: 107.333 } 
         },
         "TM25": {
             name: "Tài chính - Ngân hàng (Tài chính - Ngân hàng thương mại - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.4, hsa: 102.667 } 
         },
         "TM26": {
             name: " Tài chính - Ngân hàng (Tài chính công)",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.2, hsa: 101.333 } 
         },
         "TM27": {
             name: "Tài chính - Ngân hàng (Công nghệ tài chính ngân hàng) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25, hsa: 100 } 
         },
         "TM28": {
             name: "Thương mại điện tử (Quản trị thương mại điện tử) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 27.6, hsa: 117.333 } 
         },
         "TM29": {
             name: "Thương mại điện tử (Thương mại điện tử - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.5, hsa: 110 } 
         },
         "TM30": {
             name: "Thương mại điện tử (Kinh doanh số) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 27.2, hsa: 114.667 } 
         },
         "TM31": {
             name: "Hệ thống thông tin quản lý (Quản trị Hệ thống thông tin) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.8, hsa: 99.12 } 
         },
         "TM32": {
             name: "Hệ thống thông tin quản lý (Quản trị Hệ thống thông tin - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.2, hsa: 96.48 } 
         },
         "TM33": {
             name: "Ngôn ngữ Anh (Tiếng Anh thương mại) ",
             combs: ["A00","A01","D01","D07"],
             condition: { subject: "m-anh", min: 7.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.1, hsa: null } 
         },
         "TM34": {
             name: "Luật kinh tế (Luật kinh tế) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.4, hsa: null } 
         },
         "TM35": {
             name: " Luật kinh tế (Luật kinh doanh - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP)",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.7, hsa: null } 
         },
         "TM36": {
             name: "Luật kinh tế (Luật thương mại quốc tế) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.1, hsa: null } 
         },
         "TM37": {
             name: "Quản trị nhân lực (Quản trị nhân lực doanh nghiệp) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.6, hsa: 104 } 
         },
         "TM38": {
             name: "Quản trị nhân lực (Quản trị nhân lực doanh nghiệp - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
             condition: { subject: "m-anh", min: 6.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.8, hsa: 99.12 } 
         },
         "TM39": {
             name: "Kinh tế số (Phân tích kinh doanh trong môi trường số) ",
             combs: ["A00","A01","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 25.1, hsa: 100.667 } 
         },
         "TM40": {
             name: "Ngôn ngữ Trung Quốc (Tiếng Trung thương mại) ",
             combs: ["A00","D04","D01","D07"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 27.4, hsa: null } 
         },
         "TM41": {
             name: "Ngôn ngữ Trung Quốc (Tiếng Trung thương mại - Chương trình định hướng chuyên sâu nghề nghiệp quốc tế - IPOP) ",
             combs: ["D04"],
             condition: { subject: "m-trung", min: 8.0 },
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 26.8, hsa: null } 
         },
         "TM42": {
             name: " Quản trị kinh doanh (Tiếng Pháp thương mại)",
             combs: ["A00","A01","D01","D07","D03"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 22.5, hsa: 89 } 
         },
         "TM43": {
             name: "Quản trị kinh doanh (Quản trị kinh doanh) - Khởi nghiệp (Thành lập doanh nghiệp) - Chương trình đào tạo song bằng quốc tế ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 22.5, hsa: 89 } 
         },
         "TM44": {
             name: "Marketing (Marketing thương mại) - Thương mại sản phẩm và dịch vụ (Bán hàng) - Chương trình đào tạo song bằng quốc tế ",
             combs: ["A01","D01","D07","D09","D10","X25","X26","X27","X28"],
-            scores: { combined: null, hsa: null } 
+            scores: { combined: 24.8, hsa: 99.12 } 
         },
         "TM45": {
             name: "Quản trị kinh doanh (Quản trị kinh doanh - Chương trình đào tạo tiến tiến) ",
