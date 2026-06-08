067804d7ca12e493bda353a719b5d2dd779f41b2
Quyet2811 <nguyentquyet007@gmail.com>
2026-06-09 00:12:44 +0700

    Update and rename ussh to ussh.js

---
+++ db/ussh.js
@@ -0,0 +1,52 @@
+UNIVERSITY_CERTIFICATES["TMU"] = {
+    "IELTS": {
+        lang: "m-anh", 
+        levels: {
+            "5.5": { score: 8.0, text: "IELTS 5.5 ( Quy đổi 8.0đ )" },
+            "6.0": { score: 8.5, text: "IELTS 6.0 ( Quy đổi 8.5đ )" },
+            "6.5": { score: 9.0, text: "IELTS 6.5 ( Quy đổi 9.0đ )" },
+            "7.0": { score: 9.5, text: "IELTS 7.0 ( Quy đổi 9.5đ )" },
+            "7.5":{ score: 10.0, text:" IELTS >= 7.5 ( Quy đổi 10đ )" }
+        }
+    },
+    "TOEFL iBT": {
+        lang:"m-anh",
+        levels: {
+            "72-78": { score: 8.0, text: "TOEFL iBT 72-78 ( Quy đổi 8.0đ )" },
+            "79-87": { score: 8.5, text: "TOEFL iBT 79-87 ( Quy đổi 8.5đ )" },
+            "88-95": { score: 9.0, text: "TOEFL iBT 88-95 ( Quy đổi 9.0đ )" },
+            "96-101": { score: 9.5, text: "TOEFL iBT 96-101 ( Quy đổi 9.5đ )" },
+            "102-120":{ score: 10.0, text:" TOEFL iBT >=102 ( Quy đổi 10đ )" }
+        }
+    },
+    "HSK và HSKK": {
+        lang:"m-trung",
+        levels: {
+           "HSK 4 (180-220)": { score: 8.0, text: "HSK 4 (180-220) và HSKK Trung cấp ( Quy đổi 8.0đ )" },
+            "HSK 4 (221-260)": { score: 8.5, text: "HSK 4 (221-260) và HSKK Trung cấp ( Quy đổi 8.5đ )" },
+            "HSK 4 (261-300)": { score: 9.0, text: "HSK 4 (261-300) và HSKK Trung cấp ( Quy đổi 9.0đ )" },
+            "HSK 5": { score: 9.5, text: "HSK 5 và HSKK Cao cấp ( Quy đổi 9.5đ )" },
+            "HSK 6":{ score: 10.0, text:" HSK 6 và HSKK Cao cấp ( Quy đổi 10đ )" }
+        }
+    },
+    "JLPT": {
+        lang:"m-nhat",
+        levels: {
+           "N3 (95-124)": { score: 8.0, text: "N3 (95-124) ( Quy đổi 8.0đ )" },
+            "N3 (125-154)": { score: 8.5, text: "N3 (125-154) ( Quy đổi 8.5đ )" },
+            "N3 (155-180)": { score: 9.0, text: "N3 (155-180) ( Quy đổi 9.0đ )" },
+            "N2": { score: 9.5, text: "N2 ( Quy đổi 9.5đ )" },
+            "N1":{ score: 10.0, text:" N1 ( Quy đổi 10đ )" }
+        }
+    },
+    "TOPIK II": {
+        lang:"m-han",
+        levels: {
+           "Cấp 4 (150-163)": { score: 8.0, text: "Cấp 4 (150-163) ( Quy đổi 8.0đ )" },
+            "Cấp 4 (164-176)": { score: 8.5, text: "Cấp 4 (164-176) ( Quy đổi 8.5đ )" },
+            "Cấp 4 (177-189)": { score: 9.0, text: "Cấp 4 (177-189) ( Quy đổi 9.0đ )" },
+            "Cấp 5": { score: 9.5, text: "Cấp 5 ( Quy đổi 9.5đ )" },
+            "Cấp 6":{ score: 10.0, text:"Cấp 6 ( Quy đổi 10đ )" }
+        }
+    }
+};
