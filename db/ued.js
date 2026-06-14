6b688ad060c4555724dedf187221ffe9a5c1a650
Quyet2811 <nguyentquyet007@gmail.com>
2026-06-14 23:14:39 +0700

    Update ued.js

--- db/ued.js
+++ db/ued.js
@@ -1,50 +1,79 @@
 UNIVERSITY_CERTIFICATES["UED"] = {
-    "IELTS": {
-        lang: "m-anh", 
-        levels: {
-            "5.5": { score: 8.0, text: "IELTS 5.5 (Quy đổi 8.0đ )" },
-            "6.0": { score: 8.5, text: "IELTS 6.0 (Quy đổi 8.5đ )" },
-            "6.5": { score: 9.0, text: "IELTS 6.5 (Quy đổi 9.0đ )" },
-            "7.0": { score: 9.5, text: "IELTS 7.0 (Quy đổi 9.5đ )" },
-            "7.5":{ score: 10.0, text:" IELTS >= 7.5 (Quy đổi 10đ )" }
-        }
-    },
-    "TOEFL iBT": {
-      lang: "m-anh",
-      levels: {
-        "72-78":{ score:8.0, text:"TOEFL iBT (Quy đổi 8.0đ )"},
-        "79-87":{ score:8.5, text:"TOEFL iBT (Quy đổi 8.5đ )"},
-        "88-95":{ score:9.0, text:"TOEFL iBT (Quy đổi 9.0đ )"},
-        "96-101":{ score:9.5, text:"TOEFL iBT (Quy đổi 9.5đ )"},
-        "102-120":{ score:10.0, text:"TOEFL iBT (Quy đổi 10.0đ )"}
-        }
-    }
+    
 };
 UNIVERSITY_DATABASE["UED"] = {
     name: "Trường Đại học Giáo dục (ĐHQGHN)",
-    
-    // 1. Báo trạng thái HSA đang chờ cập nhật
     hsaConfig: {
         status: "updating", 
         calculate: function(rawHsa, bonusUt, certBonus) {
-            // Do chưa có barem chính thức năm nay nên tạm thời trả về 0
             return 0; 
         }
     },
-
-    // 2. Định nghĩa công thức tính điểm THPT ĐẶC THÙ cho riêng HUS
-    // Nếu các trường khác không có block này, hệ thống sẽ tự dùng công thức mặc định
     thptConfig: {
         calculate: function(totalThreeSubjects, actualBonusUt, certBonusThpt) {
-            // Với HUS, điểm xét tuyển bằng Tổng 3 môn + Ưu tiên + Điểm cộng từ chứng chỉ (bonus)
             return totalThreeSubjects + actualBonusUt + certBonusThpt;
         }
     },
-
-    // 3. Danh sách các ngành học
     industries: {
-      "7380101": {
-        name: "Luật",
-        combs: ["A01","A07","C01","C02","C03","C04","D01","D03","D14","D15"],
-        scores: { combined:24.2,hsa: null }
+      "QHS01": {
+        name: "Sư phạm Toán học",
+        combs: ["A00","A01","B00","B03","C01","C02","D01"],
+        scores: { combined:28.57,hsa: null }
       },
+        "QHS02": {
+        name: "Sư phạm Vật lý",
+        combs: ["A00","A01","A02","C01"],
+        scores: { combined:28,hsa: null }
+      },
+        "QHS03": {
+        name: "Sư phạm Hóa học",
+        combs: ["A00","B00","C02","D07"],
+        scores: { combined:27.74,hsa: null }
+      },
+        "QHS04": {
+        name: "Sư phạm Sinh học",
+        combs: ["A02","B00","B03","B08"],
+        scores: { combined:25.37,hsa: null }
+      },
+        "QHS05": {
+        name: "Sư phạm Khoa học Tự nhiên",
+        combs: ["A00","A02","B00"],
+        scores: { combined:25.58,hsa: null }
+      },
+        "QHS06": {
+        name: "Sư phạm Ngữ văn",
+        combs: ["D14","C04","C03","C00","D15","D01"],
+        scores: { combined:28.45,hsa: null }
+      },
+        "QHS07": {
+        name: "Sư phạm Lịch sử",
+        combs: ["A07","C00","C03","D09","D14"],
+        scores: { combined:28.99,hsa: null }
+      },
+        "QHS08": {
+        name: "Sư phạm Lịch sử - Địa lý",
+        combs: ["A07","C00"],
+        scores: { combined:29.84,hsa: null }
+      },
+        "QHS09": {
+        name: "Giáo dục Mầm non",
+        combs: ["A00","C00","C03","C04","C01","C02","D01","D04"],
+        scores: { combined:27.8,hsa: null }
+      },
+        "QHS10": {
+        name: "Giáo dục Tiểu học",
+        combs: ["A00","B00","C00","C01","C02","C03","C04","D01","D04"],
+        scores: { combined:28.6,hsa: null }
+      },
+        "QHS11": {
+        name: "Nhóm ngành Khoa học Giáo dục và khác",
+        combs: ["A00","A01","B00","C00","C01","C02","C03","C04","D01","D04"],
+        scores: { combined:25.57,hsa: null }
+      },
+        "QHS12": {
+        name: "Nhóm ngành Tâm lý học, Tâm lý học Giáo dục",
+        combs: ["A00","A01","B00","C00","C01","C02","C03","C04","D01","D04"],
+        scores: { combined:25.75,hsa: null }
+        }
+    }
+};
