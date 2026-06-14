104afe2658aaa9771aa7573a203346145f42cdcc
Quyet2811 <nguyentquyet007@gmail.com>
2026-06-15 01:35:09 +0700

    Update ulis.js

--- db/ulis.js
+++ db/ulis.js
@@ -18,6 +18,129 @@ UNIVERSITY_CERTIFICATES["ULIS"] = {
         "96-101":{ score:9.5, text:"TOEFL iBT (Quy đổi 9.5đ )"},
         "102-120":{ score:10.0, text:"TOEFL iBT (Quy đổi 10.0đ )"}
         }
+    },
+     "VSTEP 3-5": {
+      lang: "m-anh",
+      levels: {
+        "7":{ score:8.0, text:"VSTEP 7.0 (Quy đổi 8.0đ )"},
+        "7.5":{ score:8.5, text:"VSTEP 7.5 (Quy đổi 8.5đ )"},
+        "8":{ score:9.0, text:"VSTEP 8.0 (Quy đổi 9.0đ )"},
+        "8.5":{ score:9.5, text:"VSTEP 8.5 (Quy đổi 9.5đ )"},
+        "9-10":{ score:10.0, text:"VSTEP 9-10 (Quy đổi 10.0đ )"}
+        }
+    },
+     "HSK và HSKK": {
+      lang: "m-trung",
+      levels: {
+        "HSK 5(180-210)":{ score:8.0, text:"HSK5(180-210) và HSKK Cao cấp (Quy đổi 8.0đ )"},
+        "HSK 5(211-240)":{ score:8.5, text:"HSK5(211-240) và HSKK Cao cấp (Quy đổi 8.5đ )"},
+        "HSK 5(241-270)":{ score:9.0, text:"HSK5(241-270) và HSKK Cao cấp (Quy đổi 9.0đ )"},
+        "HSK 5(271-300)":{ score:9.5, text:"HSK5(271-300) và HSKK Cao cấp (Quy đổi 9.5đ )"},
+        "HSK 6":{ score:10.0, text:"HSK6 và HSKK Cao cấp (Quy đổi 10.0đ )"}
+        }
+    },
+    "TRKI": {
+      lang: "m-nga",
+      levels: {
+        "TRKI-2(436-479)":{ score:8.0, text:"TRKI-2(436-479) (Quy đổi 8.0đ )"},
+        "TRKI-2(480-524)":{ score:8.5, text:"TRKI-2(480-524) (Quy đổi 8.5đ )"},
+        "TRKI-2(525-568)":{ score:9.0, text:"TRKI-2(525-568) (Quy đổi 9.0đ )"},
+        "TRKI-2(569-612)":{ score:9.5, text:"TRKI-2(569-612) (Quy đổi 9.5đ )"},
+        "TRKI-2(613-660) và TRKI-3(429-660)":{ score:10.0, text:"TRKI-2(613-660) và TRKI-3(429-660) (Quy đổi 10.0đ )"}
+        }
+    },
+    "TCF": {
+      lang: "m-phap",
+      levels: {
+        "400-409":{ score:8.0, text:"TCF 400-409 (Quy đổi 8.0đ )"},
+        "410-419":{ score:8.5, text:"TCF 410-419 (Quy đổi 8.5đ )"},
+        "420-439":{ score:9.0, text:"TCF 420-439 (Quy đổi 9.0đ )"},
+        "440-449":{ score:9.5, text:"TCF 440-449 (Quy đổi 9.5đ )"},
+        ">=450":{ score:10.0, text:"TCF >=450 (Quy đổi 10.0đ )"}
+        }
+    },
+    "DELF": {
+      lang: "m-phap",
+      levels: {
+        "DELF B2(50-54.5)":{ score:8.0, text:"DELF B2(50-54.5) (Quy đổi 8.0đ )"},
+        "DELF B2(55-59.5)":{ score:8.5, text:"DELF B2(55-59.5) (Quy đổi 8.5đ )"},
+        "DELF B2(60-64.5)":{ score:9.0, text:"DELF B2(60-64.5) (Quy đổi 9.0đ )"},
+        "DELF B2(65-69.5)":{ score:9.5, text:"DELF B2(65-69.5) (Quy đổi 9.5đ )"},
+        "DELF B2(>=70)":{ score:10.0, text:"DELF B2(>=70) (Quy đổi 10.0đ )"}
+        }
+    },
+    "JLPT": {
+      lang: "m-nhat",
+      levels: {
+        "N3 (95-103)":{ score:8.0, text:"N3 (95-103) (Quy đổi 8.0đ )"},
+        "N3 (104-160)":{ score:8.5, text:"N3 (104-160) (Quy đổi 8.5đ )"},
+        "N3 (161-180) và N2 (90-111)":{ score:9.0, text:"N3 (161-180) và N2 (90-111) (Quy đổi 9.0đ )"},
+        "N2 (122-180) và N1 (100-141)":{ score:9.5, text:"N2 (122-180) và N1 (100-141) (Quy đổi 9.5đ )"},
+        "N1 (142-180)":{ score:10.0, text:"N1 (142-180) (Quy đổi 10.0đ )"}
+        }
+    },
+    "TOPIK": {
+        lang: "m-han",
+        levels: {
+            "Cấp 4(150-157)":{ score:8.0, text:"Cấp 4(150-157) (Quy đổi 8.0đ)"},
+            "Cấp 4(158-165)":{ score:8.5, text:"Cấp 4(158-165) (Quy đổi 8.5đ)"},
+            "Cấp 4(166-173)":{ score:9.0, text:"Cấp 4(166-173) (Quy đổi 9.0đ)"},
+            "Cấp 4(174-181)":{ score:9.5, text:"Cấp 4(174-181) (Quy đổi 9.5đ)"},
+            "Cấp 4(182-300)":{ score:10, text:"Cấp 4(182-300) (Quy đổi 10đ)"}
+        }
+    },
+    "DSH": {
+        lang: "m-duc",
+        levels: {
+            "DSH 1":{ score:8.0, text:"DSH 1 (Quy đổi 8.0đ)"},
+            "DSH 2":{ score:9.5, text:"DSH 2 (Quy đổi 9.5đ)"},
+            "DSH 3":{ score:10, text:"DSH 3 (Quy đổi 10đ)"}
+        }
+    },
+    "DSD": {
+        lang: "m-duc",
+        levels: {
+            "B2":{ score:9.5, text:"DSD B2 (Quy đổi 9.5đ)"},
+            "C1":{ score:10, text:"DSD C1 (Quy đổi 10đ)"}
+        }
+    },
+    "TestDaF (TND)":{
+        lang: "m-duc",
+        levels: {
+            "TDN 3":{ score:8.0, text:"TDN 3 (Quy đổi 8.0đ)"},
+            "TDN 4":{ score:9.5, text:"TDN 4 (Quy đổi 9.5đ)"},
+            "TDN 5":{ score:10, text:"TDN 5 (Quy đổi 10đ)"}
+        }
+    },
+    "Goethe-Zertifikat":{
+        lang: "m-duc",
+        levels: {
+            "B2 (272-303)":{ score:8.0, text:"B2 (272-303) (Quy đổi 8.0đ)"},
+            "B2 (304-355)":{ score:8.5, text:"B2 (304-355) (Quy đổi 8.5đ)"},
+            "B2 (336-367)":{ score:9.0, text:"B2 (336-367) (Quy đổi 9.0đ)"},
+            "B2 (368-400)":{ score:9.5, text:"B2 (368-400) (Quy đổi 9.5đ)"},
+            "C1 (>=240)":{ score:10, text:"C1 (>=240) (Quy đổi 10đ)"}
+        }
+    },
+    "TELC":{
+        lang: "m-duc",
+        levels: {
+            "B2 (180-203)":{ score:8.0, text:"B2 (180-203) (Quy đổi 8.0đ)"},
+            "B2 (204-227)":{ score:8.5, text:"B2 (204-227) (Quy đổi 8.5đ)"},
+            "B2 (228-251)":{ score:9.0, text:"B2 (228-251) (Quy đổi 9.0đ)"},
+            "B2 (252-300)":{ score:9.5, text:"B2 (252-300) (Quy đổi 9.5đ)"},
+            "C1 (>=180)":{ score:10, text:"B2 (>=180) (Quy đổi 10đ)"}
+        }
+    },
+    "OSD Zertifikat":{
+        lang: "m-duc",
+        levels: {
+            "B2 (180-203)":{ score:8.0, text:"B2 (180-203) (Quy đổi 8.0đ)"},
+            "B2 (204-227)":{ score:8.5, text:"B2 (204-227) (Quy đổi 8.5đ)"},
+            "B2 (228-251)":{ score:9.0, text:"B2 (228-251) (Quy đổi 9.0đ)"},
+            "B2 (252-300)":{ score:9.5, text:"B2 (252-300) (Quy đổi 9.5đ)"},
+            "C1 (>=180)":{ score:10, text:"B2 (>=180) (Quy đổi 10đ)"}
+        }
     }
 };
 UNIVERSITY_DATABASE["ULIS"] = {
@@ -34,8 +157,80 @@ UNIVERSITY_DATABASE["ULIS"] = {
         }
     },
     industries: {
-      "CN01": {
-        name: "Công nghệ thông tin",
-        combs: ["A00","A01","X06"],
-        scores: { combined:28.19,hsa: null }
+      "7140231": {
+        name: "Sư phạm tiếng Anh",
+        combs: ["A01","D01","D14","D15","D07","D08"],
+        scores: { combined:30,hsa: null }
       },
+        "7140234": {
+        name: "Sư phạm tiếng Trung Quốc",
+        combs: ["A01","D01","D14","D15","D07","D08","D04","D45","D65"],
+        scores: { combined:30,hsa: null }
+      },
+        "7140235": {
+        name: "Sư phạm tiếng Đức",
+        combs: ["A01","D01","D07","D08","D14","D15","D05","D41","D61"],
+        scores: { combined:null,hsa: null }
+      },
+        "7140236": {
+        name: "Sư phạm tiếng Nhật",
+        combs: ["A01","D01","D07","D08","D14","D15","D06","D63","D43"],
+        scores: { combined:28.1,hsa: null }
+      },
+        "7140237": {
+        name: "Sư phạm tiếng Hàn Quốc",
+        combs: ["A01","D01","D07","D08","D14","D15","DD2","DH1","DH5"],
+        scores: { combined:27.81,hsa: null }
+      },
+        "7220201": {
+        name: "Ngôn ngữ Anh",
+        combs: ["A01","D01","D07","D08","D14","D15"],
+        scores: { combined:26.85,hsa: null }
+      },
+        "7220202": {
+        name: "Ngôn ngữ Nga",
+        combs: ["A01","D01","D07","D08","D14","D15","D02","D42","D62"],
+        scores: { combined:22.6,hsa: null }
+      },
+        "7220203": {
+        name: "Ngôn ngữ Pháp",
+        combs: ["A01","D01","D07","D08","D14","D15","D03","D44","D64"],
+        scores: { combined:23.47,hsa: null }
+      },
+        "7220204": {
+        name: "Ngôn ngữ Trung Quốc",
+        combs: ["A01","D01","D14","D15","D07","D08","D04","D45","D65"],
+        scores: { combined:27.03,hsa: null }
+      },
+        "7220205": {
+        name: "Ngôn ngữ Đức",
+        combs: ["A01","D01","D07","D08","D14","D15","D05","D41","D61"],
+        scores: { combined:24.56,hsa: null }
+      },
+        "7220209": {
+        name: "Ngôn ngữ Nhật",
+        combs: ["A01","D01","D07","D08","D14","D15","D06","D63","D43"],
+        scores: { combined:23.93,hsa: null }
+      },
+        "7220210": {
+        name: "Ngôn ngữ Hàn Quốc",
+        combs:["A01","D01","D07","D08","D14","D15","DD2","DH1","DH5"],
+        scores: { combined:24.69,hsa: null }
+      },
+        "7220211": {
+        name: "Ngôn ngữ Ả rập",
+        combs: ["A01","D01","D07","D08","D14","D15"],
+        scores: { combined:21.88,hsa: null }
+      },
+        "7220212": {
+        name: "Văn hóa và truyền thông xuyên quốc gia",
+        combs: ["A01","D01","D07","D08","D14","D15"],
+        scores: { combined:24.58,hsa: null }
+      },
+        "7220101": {
+        name: "Tiếng Việt và Văn hóa Việt Nam",
+        combs: ["A01","D01","D07","D08","D14","D15"],
+        scores: { combined:22.56,hsa: null }
+      }
+    }
+};
