0fb5fa4be6177a4782d8d508739b8d93095b9dfb
Quyet2811 <nguyentquyet007@gmail.com>
2026-06-15 22:27:22 +0700

    Update ulis.js

--- db/ulis.js
+++ db/ulis.js
@@ -2,81 +2,81 @@ UNIVERSITY_CERTIFICATES["ULIS"] = {
     "IELTS": {
         lang: "m-anh", 
         levels: {
-            "5.5": { score: 8.0, text: "IELTS 5.5 (Quy đổi 8.0đ )" },
-            "6.0": { score: 8.5, text: "IELTS 6.0 (Quy đổi 8.5đ )" },
-            "6.5": { score: 9.0, text: "IELTS 6.5 (Quy đổi 9.0đ )" },
-            "7.0": { score: 9.5, text: "IELTS 7.0 (Quy đổi 9.5đ )" },
-            "7.5":{ score: 10.0, text:" IELTS >= 7.5 (Quy đổi 10đ )" }
+            "5.5": { score: 8.0, text: "IELTS 5.5 (Quy đổi 8.0đ)" },
+            "6.0": { score: 8.5, text: "IELTS 6.0 (Quy đổi 8.5đ)" },
+            "6.5": { score: 9.0, text: "IELTS 6.5 (Quy đổi 9.0đ)" },
+            "7.0": { score: 9.5, text: "IELTS 7.0 (Quy đổi 9.5đ)" },
+            "7.5":{ score: 10.0, text:" IELTS >= 7.5 (Quy đổi 10đ)" }
         }
     },
     "TOEFL iBT": {
       lang: "m-anh",
       levels: {
-        "72-78":{ score:8.0, text:"TOEFL iBT (Quy đổi 8.0đ )"},
-        "79-87":{ score:8.5, text:"TOEFL iBT (Quy đổi 8.5đ )"},
-        "88-95":{ score:9.0, text:"TOEFL iBT (Quy đổi 9.0đ )"},
-        "96-101":{ score:9.5, text:"TOEFL iBT (Quy đổi 9.5đ )"},
-        "102-120":{ score:10.0, text:"TOEFL iBT (Quy đổi 10.0đ )"}
+        "72-78":{ score:8.0, text:"TOEFL(72-78) (Quy đổi 8.0đ)"},
+        "79-87":{ score:8.5, text:"TOEFL(79-87) (Quy đổi 8.5đ)"},
+        "88-95":{ score:9.0, text:"TOEFL(88-95) (Quy đổi 9.0đ)"},
+        "96-101":{ score:9.5, text:"TOEFL(96-101) (Quy đổi 9.5đ)"},
+        "102-120":{ score:10.0, text:"TOEFL(102-120) (Quy đổi 10.0đ)"}
         }
     },
      "VSTEP 3-5": {
       lang: "m-anh",
       levels: {
-        "7":{ score:8.0, text:"VSTEP 7.0 (Quy đổi 8.0đ )"},
-        "7.5":{ score:8.5, text:"VSTEP 7.5 (Quy đổi 8.5đ )"},
-        "8":{ score:9.0, text:"VSTEP 8.0 (Quy đổi 9.0đ )"},
-        "8.5":{ score:9.5, text:"VSTEP 8.5 (Quy đổi 9.5đ )"},
-        "9-10":{ score:10.0, text:"VSTEP 9-10 (Quy đổi 10.0đ )"}
+        "7":{ score:8.0, text:"VSTEP 7.0 (Quy đổi 8.0đ)"},
+        "7.5":{ score:8.5, text:"VSTEP 7.5 (Quy đổi 8.5đ)"},
+        "8":{ score:9.0, text:"VSTEP 8.0 (Quy đổi 9.0đ)"},
+        "8.5":{ score:9.5, text:"VSTEP 8.5 (Quy đổi 9.5đ)"},
+        "9-10":{ score:10.0, text:"VSTEP 9-10 (Quy đổi 10.0đ)"}
         }
     },
      "HSK và HSKK": {
       lang: "m-trung",
       levels: {
-        "HSK 5(180-210)":{ score:8.0, text:"HSK5(180-210) và HSKK Cao cấp (Quy đổi 8.0đ )"},
-        "HSK 5(211-240)":{ score:8.5, text:"HSK5(211-240) và HSKK Cao cấp (Quy đổi 8.5đ )"},
-        "HSK 5(241-270)":{ score:9.0, text:"HSK5(241-270) và HSKK Cao cấp (Quy đổi 9.0đ )"},
-        "HSK 5(271-300)":{ score:9.5, text:"HSK5(271-300) và HSKK Cao cấp (Quy đổi 9.5đ )"},
-        "HSK 6":{ score:10.0, text:"HSK6 và HSKK Cao cấp (Quy đổi 10.0đ )"}
+        "HSK 5(180-210)":{ score:8.0, text:"HSK5(180-210) và HSKK Cao cấp (Quy đổi 8.0đ)"},
+        "HSK 5(211-240)":{ score:8.5, text:"HSK5(211-240) và HSKK Cao cấp (Quy đổi 8.5đ)"},
+        "HSK 5(241-270)":{ score:9.0, text:"HSK5(241-270) và HSKK Cao cấp (Quy đổi 9.0đ)"},
+        "HSK 5(271-300)":{ score:9.5, text:"HSK5(271-300) và HSKK Cao cấp (Quy đổi 9.5đ)"},
+        "HSK 6":{ score:10.0, text:"HSK6 và HSKK Cao cấp (Quy đổi 10.0đ)"}
         }
     },
     "TRKI": {
       lang: "m-nga",
       levels: {
-        "TRKI-2(436-479)":{ score:8.0, text:"TRKI-2(436-479) (Quy đổi 8.0đ )"},
-        "TRKI-2(480-524)":{ score:8.5, text:"TRKI-2(480-524) (Quy đổi 8.5đ )"},
-        "TRKI-2(525-568)":{ score:9.0, text:"TRKI-2(525-568) (Quy đổi 9.0đ )"},
-        "TRKI-2(569-612)":{ score:9.5, text:"TRKI-2(569-612) (Quy đổi 9.5đ )"},
-        "TRKI-2(613-660) và TRKI-3(429-660)":{ score:10.0, text:"TRKI-2(613-660) và TRKI-3(429-660) (Quy đổi 10.0đ )"}
+        "TRKI-2(436-479)":{ score:8.0, text:"TRKI-2(436-479) (Quy đổi 8.0đ)"},
+        "TRKI-2(480-524)":{ score:8.5, text:"TRKI-2(480-524) (Quy đổi 8.5đ)"},
+        "TRKI-2(525-568)":{ score:9.0, text:"TRKI-2(525-568) (Quy đổi 9.0đ)"},
+        "TRKI-2(569-612)":{ score:9.5, text:"TRKI-2(569-612) (Quy đổi 9.5đ)"},
+        "TRKI-2(613-660) và TRKI-3(429-660)":{ score:10.0, text:"TRKI-2(613-660) và TRKI-3(429-660) (Quy đổi 10.0đ)"}
         }
     },
     "TCF": {
       lang: "m-phap",
       levels: {
-        "400-409":{ score:8.0, text:"TCF 400-409 (Quy đổi 8.0đ )"},
-        "410-419":{ score:8.5, text:"TCF 410-419 (Quy đổi 8.5đ )"},
-        "420-439":{ score:9.0, text:"TCF 420-439 (Quy đổi 9.0đ )"},
-        "440-449":{ score:9.5, text:"TCF 440-449 (Quy đổi 9.5đ )"},
-        ">=450":{ score:10.0, text:"TCF >=450 (Quy đổi 10.0đ )"}
+        "400-409":{ score:8.0, text:"TCF 400-409 (Quy đổi 8.0đ)"},
+        "410-419":{ score:8.5, text:"TCF 410-419 (Quy đổi 8.5đ)"},
+        "420-439":{ score:9.0, text:"TCF 420-439 (Quy đổi 9.0đ)"},
+        "440-449":{ score:9.5, text:"TCF 440-449 (Quy đổi 9.5đ)"},
+        ">=450":{ score:10.0, text:"TCF >=450 (Quy đổi 10.0đ)"}
         }
     },
     "DELF": {
       lang: "m-phap",
       levels: {
-        "DELF B2(50-54.5)":{ score:8.0, text:"DELF B2(50-54.5) (Quy đổi 8.0đ )"},
-        "DELF B2(55-59.5)":{ score:8.5, text:"DELF B2(55-59.5) (Quy đổi 8.5đ )"},
-        "DELF B2(60-64.5)":{ score:9.0, text:"DELF B2(60-64.5) (Quy đổi 9.0đ )"},
-        "DELF B2(65-69.5)":{ score:9.5, text:"DELF B2(65-69.5) (Quy đổi 9.5đ )"},
-        "DELF B2(>=70)":{ score:10.0, text:"DELF B2(>=70) (Quy đổi 10.0đ )"}
+        "DELF B2(50-54.5)":{ score:8.0, text:"DELF B2(50-54.5) (Quy đổi 8.0đ)"},
+        "DELF B2(55-59.5)":{ score:8.5, text:"DELF B2(55-59.5) (Quy đổi 8.5đ)"},
+        "DELF B2(60-64.5)":{ score:9.0, text:"DELF B2(60-64.5) (Quy đổi 9.0đ)"},
+        "DELF B2(65-69.5)":{ score:9.5, text:"DELF B2(65-69.5) (Quy đổi 9.5đ)"},
+        "DELF B2(>=70)":{ score:10.0, text:"DELF B2(>=70) (Quy đổi 10.0đ)"}
         }
     },
     "JLPT": {
       lang: "m-nhat",
       levels: {
-        "N3 (95-103)":{ score:8.0, text:"N3 (95-103) (Quy đổi 8.0đ )"},
-        "N3 (104-160)":{ score:8.5, text:"N3 (104-160) (Quy đổi 8.5đ )"},
-        "N3 (161-180) và N2 (90-111)":{ score:9.0, text:"N3 (161-180) và N2 (90-111) (Quy đổi 9.0đ )"},
-        "N2 (122-180) và N1 (100-141)":{ score:9.5, text:"N2 (122-180) và N1 (100-141) (Quy đổi 9.5đ )"},
-        "N1 (142-180)":{ score:10.0, text:"N1 (142-180) (Quy đổi 10.0đ )"}
+        "N3 (95-103)":{ score:8.0, text:"N3 (95-103) (Quy đổi 8.0đ)"},
+        "N3 (104-160)":{ score:8.5, text:"N3 (104-160) (Quy đổi 8.5đ)"},
+        "N3 (161-180) và N2 (90-111)":{ score:9.0, text:"N3 (161-180) và N2 (90-111) (Quy đổi 9.0đ)"},
+        "N2 (122-180) và N1 (100-141)":{ score:9.5, text:"N2 (122-180) và N1 (100-141) (Quy đổi 9.5đ)"},
+        "N1 (142-180)":{ score:10.0, text:"N1 (142-180) (Quy đổi 10.0đ)"}
         }
     },
     "TOPIK": {
