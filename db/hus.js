357be967ecbbafcf4017de73b94ffbd71f6b4198
Quyet2811 <nguyentquyet007@gmail.com>
2026-06-15 22:22:20 +0700

    Update hus.js

--- db/hus.js
+++ db/hus.js
@@ -4,21 +4,21 @@ UNIVERSITY_CERTIFICATES["HUS"] = {
     "IELTS": {
         lang: "m-anh", 
         levels: {
-            "5.5": { score: 8.0, text: "IELTS 5.5 (Quy đổi 8.0đ )" },
-            "6.0": { score: 8.5, text: "IELTS 6.0 (Quy đổi 8.5đ )" },
-            "6.5": { score: 9.0, text: "IELTS 6.5 (Quy đổi 9.0đ )" },
-            "7.0": { score: 9.5, text: "IELTS 7.0 (Quy đổi 9.5đ )" },
-            "7.5":{ score: 10.0, text:" IELTS >= 7.5 (Quy đổi 10đ )" }
+            "5.5": { score: 8.0, text: "IELTS 5.5 (Quy đổi 8.0đ)" },
+            "6.0": { score: 8.5, text: "IELTS 6.0 (Quy đổi 8.5đ)" },
+            "6.5": { score: 9.0, text: "IELTS 6.5 (Quy đổi 9.0đ)" },
+            "7.0": { score: 9.5, text: "IELTS 7.0 (Quy đổi 9.5đ)" },
+            "7.5":{ score: 10.0, text:" IELTS >= 7.5 (Quy đổi 10đ)" }
         }
     },
     "TOEFL iBT": {
       lang: "m-anh",
       levels: {
-        "72-78":{ score:8.0, text:"TOEFL iBT (Quy đổi 8.0đ )"},
-        "79-87":{ score:8.5, text:"TOEFL iBT (Quy đổi 8.5đ )"},
-        "88-95":{ score:9.0, text:"TOEFL iBT (Quy đổi 9.0đ )"},
-        "96-101":{ score:9.5, text:"TOEFL iBT (Quy đổi 9.5đ )"},
-        "102-120":{ score:10.0, text:"TOEFL iBT (Quy đổi 10.0đ )"}
+        "72-78":{ score:8.0, text:"TOEFL(72-78) (Quy đổi 8.0đ)"},
+        "79-87":{ score:8.5, text:"TOEFL(79-87) (Quy đổi 8.5đ)"},
+        "88-95":{ score:9.0, text:"TOEFL(88-95) (Quy đổi 9.0đ)"},
+        "96-101":{ score:9.5, text:"TOEFL(96-101) (Quy đổi 9.5đ)"},
+        "102-120":{ score:10.0, text:"TOEFL(102-120) (Quy đổi 10.0đ)"}
         }
     }
 }
